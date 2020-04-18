13980310d5dc6154fcad7336a81e4f509d9f9474
Angel Zlatenov <angel.zl@abv.bg>
2020-04-18 17:31:00 +0300

    Initial Commit - project setup

---
+++ src/main/java/test/zlatenov/accountmanaging/AccountManagingApplication.java
@@ -0,0 +1,13 @@
+package test.zlatenov.accountmanaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountManagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/zlatenov/accountmanaging/AccountManagingApplicationTests.java
@@ -0,0 +1,13 @@
+package test.zlatenov.accountmanaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
